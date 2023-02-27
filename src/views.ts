31ecb3f2f94cecb3bb8a707bdb61792731e13e93
Anthony Sargeant <anthony.sargeant@sky.uk>
2023-02-27 10:18:12 +0000

    Added code to make the darn tutorial work

---
+++ src/views.ts
@@ -0,0 +1,51 @@
+import { FORWARDS, START } from '@eventstore/db-client';
+import { client as eventStore } from './event-store';
+
+interface Purchase {
+  purchaseId: string;
+  name: string;
+  amount: number;
+  wasRefunded: boolean;
+}
+
+const getAllPurchases = async () => {
+  const purchases: Purchase[] = [];
+  const getPurchaseById = createGetPurchaseById(purchases);
+
+  const events = eventStore.readAll({
+    direction: FORWARDS,
+    fromPosition: START,
+    maxCount: 1000,
+  });
+
+  for await (const { event } of events) {
+    const data: any = event.data;
+
+    switch (event?.type) {
+      case 'ProductPurchased':
+        purchases.push({
+          purchaseId: data.purchaseId,
+          amount: data.amount,
+          name: data.name,
+          wasRefunded: false,
+        });
+        console.log(`${JSON.stringify(event.data, null, 2)}`);
+        break;
+
+      case 'ProductRefunded':
+        getPurchaseById(data.purchaseId).wasRefunded = true;
+        break;
+    }
+  }
+
+  purchases.forEach((purchase) => {
+    console.log(`${JSON.stringify(purchase, null, 2)}`);
+  });
+
+  return purchases;
+};
+
+const createGetPurchaseById = (purchases: Purchase[]) => (purchaseId: string) =>
+  purchases.filter((p) => p.purchaseId == purchaseId)[0];
+
+export { getAllPurchases, Purchase };
